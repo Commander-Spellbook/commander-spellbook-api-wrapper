907420c62500bba00cfab1ab837cab8c49d1ee0d
Blade Barringer <blade@crookedneighbor.com>
2021-01-14 11:50:23 -0600

    refactor: clean up parse query test

--- test/unit/parse-query/index.test.ts
+++ test/unit/parse-query/index.test.ts
@@ -1,40 +1,44 @@
 import parseQuery from "../../../src/parse-query";
+import parseCardQuery from "../../../src/parse-query/parse-card-query";
+import parseColorIdentity from "../../../src/parse-query/parse-color-identity";
 import parseComboData from "../../../src/parse-query/parse-combo-data";
 
+jest.mock("../../../src/parse-query/parse-card-query");
+jest.mock("../../../src/parse-query/parse-color-identity");
 jest.mock("../../../src/parse-query/parse-combo-data");
 
 describe("parseQuery", () => {
   it("parses plain text into cards", () => {
-    const result = parseQuery("foo bar baz");
+    parseQuery("foo bar baz");
 
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "foo",
-            },
-            {
-              method: ":",
-              value: "bar",
-            },
-            {
-              method: ":",
-              value: "baz",
-            },
-          ],
-        },
-      })
+    expect(parseCardQuery).toBeCalledTimes(3);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "foo"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "bar"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "baz"
     );
   });
 
   // TODO this should not support this
   it("supports an empty string for query", () => {
     const result = parseQuery("");
+
+    expect(parseCardQuery).not.toBeCalled();
+    expect(parseColorIdentity).not.toBeCalled();
+    expect(parseComboData).not.toBeCalled();
 
     expect(result).toEqual({
       cards: {
@@ -68,22 +72,23 @@ describe("parseQuery", () => {
       "foo unknown:value unknown2:'value 2' unknown3:\"value 3\" bar -card>3"
     );
 
+    expect(parseCardQuery).toBeCalledTimes(3);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "foo"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "bar"
+    );
+    expect(parseCardQuery).toBeCalledWith(expect.anything(), "-card", ">", "3");
+
     expect(result).toEqual(
       expect.objectContaining({
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "foo",
-            },
-            {
-              method: ":",
-              value: "bar",
-            },
-          ],
-        },
         errors: [
           {
             key: "unknown",
@@ -100,11 +105,6 @@ describe("parseQuery", () => {
             value: "value 3",
             message: 'Could not parse keyword "unknown3" with value "value 3"',
           },
-          {
-            key: "-card",
-            value: "3",
-            message: 'The key "-card" does not support operator ">"',
-          },
         ],
       })
     );
@@ -113,6 +113,38 @@ describe("parseQuery", () => {
   it("can parse a mix of all queries", () => {
     const result = parseQuery(
       "Kiki ci:wbr -ci=br card:Daxos id:12345 card:'Grave Titan' card:\"Akroma\" unknown:value -card:Food prerequisites:prereq steps:step results:result -prerequisites:xprereq -steps:xstep -result:xresult"
+    );
+
+    expect(parseCardQuery).toBeCalledTimes(5);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Kiki"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Daxos"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Grave Titan"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Akroma"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "-card",
+      ":",
+      "Food"
     );
 
     expect(parseComboData).toBeCalledTimes(6);
@@ -153,51 +185,23 @@ describe("parseQuery", () => {
       "xresult"
     );
 
+    expect(parseColorIdentity).toBeCalledTimes(2);
+    expect(parseColorIdentity).toBeCalledWith(
+      expect.anything(),
+      "ci",
+      ":",
+      "wbr"
+    );
+    expect(parseColorIdentity).toBeCalledWith(
+      expect.anything(),
+      "-ci",
+      "=",
+      "br"
+    );
+
     expect(result).toEqual(
       expect.objectContaining({
         id: "12345",
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [
-            {
-              method: ":",
-              value: "Food",
-            },
-          ],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Kiki",
-            },
-            {
-              method: ":",
-              value: "Daxos",
-            },
-            {
-              method: ":",
-              value: "Grave Titan",
-            },
-            {
-              method: ":",
-              value: "Akroma",
-            },
-          ],
-        },
-        colorIdentity: {
-          includeFilters: [
-            {
-              method: ":",
-              value: ["w", "b", "r"],
-            },
-          ],
-          excludeFilters: [
-            {
-              method: "=",
-              value: ["b", "r"],
-            },
-          ],
-          sizeFilters: [],
-        },
         errors: [
           {
             key: "unknown",
@@ -214,6 +218,38 @@ describe("parseQuery", () => {
       "Kiki CI:wbr CARD:Daxos ID:12345 CARD:'Grave Titan' CARD:\"Akroma\" UNKNOWN:value -CARD:Food PREREQUISITES:prereq STEPS:step RESULTS:result -PREREQUISITES:xprereq -STEPS:xstep -RESULT:xresult"
     );
 
+    expect(parseCardQuery).toBeCalledTimes(5);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Kiki"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Daxos"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Grave Titan"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Akroma"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "-card",
+      ":",
+      "Food"
+    );
+
     expect(parseComboData).toBeCalledTimes(6);
     expect(parseComboData).toBeCalledWith(
       expect.anything(),
@@ -252,46 +288,17 @@ describe("parseQuery", () => {
       "xresult"
     );
 
+    expect(parseColorIdentity).toBeCalledTimes(1);
+    expect(parseColorIdentity).toBeCalledWith(
+      expect.anything(),
+      "ci",
+      ":",
+      "wbr"
+    );
+
     expect(result).toEqual(
       expect.objectContaining({
         id: "12345",
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [
-            {
-              method: ":",
-              value: "Food",
-            },
-          ],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Kiki",
-            },
-            {
-              method: ":",
-              value: "Daxos",
-            },
-            {
-              method: ":",
-              value: "Grave Titan",
-            },
-            {
-              method: ":",
-              value: "Akroma",
-            },
-          ],
-        },
-        colorIdentity: {
-          includeFilters: [
-            {
-              method: ":",
-              value: ["w", "b", "r"],
-            },
-          ],
-          excludeFilters: [],
-          sizeFilters: [],
-        },
         errors: [
           {
             key: "unknown",
@@ -308,6 +315,38 @@ describe("parseQuery", () => {
       "Kiki c_i:wbr c_ar_d:Daxos i_d:12345 ca_rd:'Grave Titan' ca_rd:\"Akroma\" unknow_n:value -c_ard:Food _prere_quisit_es_:prereq st_eps:step r_esu_lts:result -prer_equisites:xprereq -ste_ps:xstep -res_ult:xresult"
     );
 
+    expect(parseCardQuery).toBeCalledTimes(5);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Kiki"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Daxos"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Grave Titan"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Akroma"
+    );
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "-card",
+      ":",
+      "Food"
+    );
+
     expect(parseComboData).toBeCalledTimes(6);
     expect(parseComboData).toBeCalledWith(
       expect.anything(),
@@ -346,46 +385,17 @@ describe("parseQuery", () => {
       "xresult"
     );
 
+    expect(parseColorIdentity).toBeCalledTimes(1);
+    expect(parseColorIdentity).toBeCalledWith(
+      expect.anything(),
+      "ci",
+      ":",
+      "wbr"
+    );
+
     expect(result).toEqual(
       expect.objectContaining({
         id: "12345",
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [
-            {
-              method: ":",
-              value: "Food",
-            },
-          ],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Kiki",
-            },
-            {
-              method: ":",
-              value: "Daxos",
-            },
-            {
-              method: ":",
-              value: "Grave Titan",
-            },
-            {
-              method: ":",
-              value: "Akroma",
-            },
-          ],
-        },
-        colorIdentity: {
-          includeFilters: [
-            {
-              method: ":",
-              value: ["w", "b", "r"],
-            },
-          ],
-          excludeFilters: [],
-          sizeFilters: [],
-        },
         errors: [
           {
             key: "unknown",
@@ -408,265 +418,74 @@ describe("parseQuery", () => {
     );
   });
 
-  it("parses ci query into colorIdentity", () => {
-    const result = parseQuery("ci:wbr -ci:br");
+  it.each(["ci", "coloridentity"])(
+    "parses %s into color identity parses",
+    (kind) => {
+      parseQuery(`${kind}:wbr -${kind}:br`);
 
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        colorIdentity: {
-          includeFilters: [
-            {
-              method: ":",
-              value: ["w", "b", "r"],
-            },
-          ],
-          excludeFilters: [
-            {
-              method: ":",
-              value: ["b", "r"],
-            },
-          ],
-          sizeFilters: [],
-        },
-      })
+      expect(parseColorIdentity).toBeCalledTimes(2);
+      expect(parseColorIdentity).toBeCalledWith(
+        expect.anything(),
+        kind,
+        ":",
+        "wbr"
+      );
+      expect(parseColorIdentity).toBeCalledWith(
+        expect.anything(),
+        `-${kind}`,
+        ":",
+        "br"
+      );
+    }
+  );
+
+  it.each(["card", "cards"])("parses %s into card query parser", (kind) => {
+    parseQuery(
+      `${kind}:Rashmi ${kind}:"with quotes", -${kind}:'excluded thing'`
     );
-  });
 
-  it("parses coloridentity query into colorIdentity", () => {
-    const result = parseQuery("coloridentity:wbr -coloridentity:br");
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        colorIdentity: {
-          includeFilters: [
-            {
-              method: ":",
-              value: ["w", "b", "r"],
-            },
-          ],
-          excludeFilters: [
-            {
-              method: ":",
-              value: ["b", "r"],
-            },
-          ],
-          sizeFilters: [],
-        },
-      })
+    expect(parseCardQuery).toBeCalledTimes(3);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      kind,
+      ":",
+      "Rashmi"
     );
-  });
-
-  it("parses card query into cards", () => {
-    const result = parseQuery("card:Rashmi");
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Rashmi",
-            },
-          ],
-        },
-      })
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      kind,
+      ":",
+      "with quotes"
     );
-  });
-
-  it("parses cards query into cards", () => {
-    const result = parseQuery("cards:Rashmi");
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Rashmi",
-            },
-          ],
-        },
-      })
-    );
-  });
-
-  it("can parse multiple card queries", () => {
-    const result = parseQuery("card:Rashmi cards:Arjun card:Sydri");
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Rashmi",
-            },
-            {
-              method: ":",
-              value: "Arjun",
-            },
-            {
-              method: ":",
-              value: "Sydri",
-            },
-          ],
-        },
-      })
-    );
-  });
-
-  it("can parse full names with double quotes", () => {
-    const result = parseQuery('card:"Rashmi, Eternities Crafter"');
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Rashmi, Eternities Crafter",
-            },
-          ],
-        },
-      })
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      `-${kind}`,
+      ":",
+      "excluded thing"
     );
   });
 
   it("can parse names with apostrophes", () => {
-    const result = parseQuery(`card:"Freyalise, Llanowar's Fury"`);
+    parseQuery(`card:"Freyalise, Llanowar's Fury"`);
 
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Freyalise, Llanowar's Fury",
-            },
-          ],
-        },
-      })
+    expect(parseCardQuery).toBeCalledTimes(1);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      "Freyalise, Llanowar's Fury"
     );
   });
 
   it("can parse names with quotes in the name", () => {
-    const result = parseQuery(`card:'Kongming, "Sleeping Dragon"'`);
+    parseQuery(`card:'Kongming, "Sleeping Dragon"'`);
 
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: `Kongming, "Sleeping Dragon"`,
-            },
-          ],
-        },
-      })
-    );
-  });
-
-  it("can parse full names with single quotes", () => {
-    const result = parseQuery("card:'Rashmi, Eternities Crafter'");
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Rashmi, Eternities Crafter",
-            },
-          ],
-        },
-      })
-    );
-  });
-
-  it("can parse multiple card query types", () => {
-    const result = parseQuery(
-      "card:'Rashmi, Eternities Crafter' cards:Arjun card:'Sydri, Galvanic'"
-    );
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [],
-          includeFilters: [
-            {
-              method: ":",
-              value: "Rashmi, Eternities Crafter",
-            },
-            {
-              method: ":",
-              value: "Arjun",
-            },
-            {
-              method: ":",
-              value: "Sydri, Galvanic",
-            },
-          ],
-        },
-      })
-    );
-  });
-
-  it("parses negative card query into cards", () => {
-    const result = parseQuery(
-      "-card:Rashmi -card:Arjun -cards:\"Food\" -card:'Sydri'"
-    );
-
-    expect(result).toEqual(
-      expect.objectContaining({
-        errors: [],
-        cards: {
-          sizeFilters: [],
-          excludeFilters: [
-            {
-              method: ":",
-              value: "Rashmi",
-            },
-            {
-              method: ":",
-              value: "Arjun",
-            },
-            {
-              method: ":",
-              value: "Food",
-            },
-            {
-              method: ":",
-              value: "Sydri",
-            },
-          ],
-          includeFilters: [],
-        },
-      })
+    expect(parseCardQuery).toBeCalledTimes(1);
+    expect(parseCardQuery).toBeCalledWith(
+      expect.anything(),
+      "card",
+      ":",
+      `Kongming, "Sleeping Dragon"`
     );
   });
 
