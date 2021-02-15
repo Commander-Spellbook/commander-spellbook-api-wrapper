0bd3ad688f7df3db1dee95efe6b8f1733948815c
Blade Barringer <blade@crookedneighbor.com>
2021-02-15 11:04:27 -0600

    feat: add parse sort

---
+++ src/parse-query/parse-sort.ts
@@ -0,0 +1,56 @@
+import type { SearchParameters } from "../types";
+import normalizeStringInput from "../normalize-string-input";
+
+export default function parseSort(
+  params: SearchParameters,
+  value: string
+): void {
+  if (params.sort) {
+    params.errors.push({
+      key: "sort",
+      value,
+      message: `Sort option "${params.sort}" already chosen. Sorting by "${value}" will be ignored.`,
+    });
+
+    return;
+  }
+
+  value = normalizeStringInput(value);
+
+  switch (value) {
+    case "results":
+    case "numberofresults":
+      value = "number-of-results";
+      break;
+    case "steps":
+    case "numberofsteps":
+      value = "number-of-steps";
+      break;
+    case "prerequisites":
+    case "numberofprerequisites":
+      value = "number-of-prerequisites";
+      break;
+    case "cards":
+    case "numberofcards":
+      value = "number-of-cards";
+      break;
+    case "numberofcolors":
+      value = "number-of-colors";
+      break;
+    case "color":
+    case "colors":
+      value = "colors";
+      break;
+    case "id":
+      break;
+    default:
+      params.errors.push({
+        key: "sort",
+        value,
+        message: `Unknown sort option "${value}".`,
+      });
+      return;
+  }
+
+  params.sort = value;
+}
---
+++ test/unit/parse-query/parse-sort.test.ts
@@ -0,0 +1,59 @@
+import parseSort from "../../../src/parse-query/parse-sort";
+import { makeSearchParams } from "../helper";
+
+import type { SearchParameters } from "../../../src/types";
+
+describe("parseSort", () => {
+  let searchParams: SearchParameters;
+
+  beforeEach(() => {
+    searchParams = makeSearchParams();
+  });
+
+  it.each([
+    "number-of-results",
+    "number-of-steps",
+    "number-of-prerequisites",
+    "number-of-cards",
+    "number-of-colors",
+    "id",
+    "colors",
+  ])("suports %s", (kind) => {
+    parseSort(searchParams, kind);
+
+    expect(searchParams.sort).toEqual(kind);
+  });
+
+  it.each(["results", "steps", "prerequisites", "cards"])(
+    "supports %s (as alias)",
+    (kind) => {
+      parseSort(searchParams, kind);
+
+      expect(searchParams.sort).toEqual(`number-of-${kind}`);
+    }
+  );
+
+  it("provides error if invalid valud is ued for sort", () => {
+    parseSort(searchParams, "foo");
+
+    expect(searchParams.sort).toBeFalsy();
+    expect(searchParams.errors[0]).toEqual({
+      key: "sort",
+      value: "foo",
+      message: `Unknown sort option "foo".`,
+    });
+  });
+
+  it("provides error if sort is already specified", () => {
+    parseSort(searchParams, "results");
+
+    parseSort(searchParams, "cards");
+
+    expect(searchParams.sort).toEqual("number-of-results");
+    expect(searchParams.errors[0]).toEqual({
+      key: "sort",
+      value: "cards",
+      message: `Sort option "number-of-results" already chosen. Sorting by "cards" will be ignored.`,
+    });
+  });
+});
