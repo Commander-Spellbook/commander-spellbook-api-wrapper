6cbcaef331f57a6446e524d483f4f3ef81fc3934
Blade Barringer <blade@crookedneighbor.com>
2021-03-14 07:20:16 -0500

    fix: correct spacing in test

--- test/unit/parse-query/parse-color-from-value.test.ts
+++ test/unit/parse-query/parse-color-from-value.test.ts
@@ -20,88 +20,88 @@ describe("parseColorFromValue", () => {
   });
 
   it.each`
-    name            | expectedResult
-    ${"c"}          | ${"c"}
-    ${"Colorless"}  | ${"c"}
-    ${"White"}      | ${"w"}
-    ${"Blue"}       | ${"u"}
-    ${"Black"}      | ${"b"}
-    ${"Red"}        | ${"r"}
-    ${"Green"}      | ${"g"}
-    ${"MonoWhite"}  | ${"w"}
-    ${"MonoBlue"}   | ${"u"}
-    ${"MonoBlack"}  | ${"b"}
-    ${"MonoRed"}    | ${"r"}
-    ${"MonoGreen"}  | ${"g"}
-    ${"Mono White"} | ${"w"}
-    ${"Mono Blue"}  | ${"u"}
-    ${"Mono Black"} | ${"b"}
-    ${"Mono Red"}   | ${"r"}
-    ${"Mono Green"} | ${"g"}
-    ${"Mono-White"} | ${"w"}
-    ${"Mono-Blue"}  | ${"u"}
-    ${"Mono-Black"} | ${"b"}
-    ${"Mono-Red"}   | ${"r"}
-    ${"Mono-Green"} | ${"g"}
-    ${"Azorius"}    | ${"wu"}
-    ${"Dimir"}      | ${"ub"}
-    ${"Rakdos"}     | ${"br"}
-    ${"Gruul"}      | ${"rg"}
-    ${"Selesnya"}   | ${"wg"}
-    ${"Orzhov"}     | ${"wb"}
-    ${"Izzet"}      | ${"ur"}
-    ${"Golgari"}    | ${"bg"}
-    ${"Boros"}      | ${"wr"}
-    ${"Simic"}      | ${"ug"}
-    ${"Naya"}       | ${"wrg"}
-    ${"Esper"}      | ${"wub"}
-    ${"Grixis"}     | ${"ubr"}
-    ${"Jund"}       | ${"brg"}
-    ${"Bant"}       | ${"wug"}
-    ${"Abzan"}      | ${"wbg"}
-    ${"Temur"}      | ${"urg"}
-    ${"Jeskai"}     | ${"wur"}
-    ${"Mardu"}      | ${"wbr"}
-    ${"Sultai"}     | ${"bug"}
-    ${"Glint"}      | ${"ubrg"}
-    ${"Glinteye"}   | ${"ubrg"}
-    ${"Glint Eye"}  | ${"ubrg"}
-    ${"Glint-Eye"}  | ${"ubrg"}
-    ${"Chaos"}      | ${"ubrg"}
-    ${"Dune"}       | ${"wbrg"}
-    ${"Dunebrood"}  | ${"wbrg"}
-    ${"Dune Brood"} | ${"wbrg"}
-    ${"Dune-Brood"} | ${"wbrg"}
-    ${"Aggression"} | ${"wbrg"}
-    ${"Ink"}        | ${"wurg"}
-    ${"Inktreader"} | ${"wurg"}
-    ${"Ink-Treader"}| ${"wurg"}
-    ${"Ink Treader"}| ${"wurg"}
-    ${"Altruism"}   | ${"wurg"}
-    ${"Witch"}      | ${"wubg"}
-    ${"Witchmaw"}   | ${"wubg"}
-    ${"Witch-Maw"}  | ${"wubg"}
-    ${"Witch Maw"}  | ${"wubg"}
-    ${"Growth"}     | ${"wubg"}
-    ${"Yore"}       | ${"wubr"}
-    ${"Yoretiller"} | ${"wubr"}
-    ${"Yore Tiller"}| ${"wubr"}
-    ${"Yore-Tiller"}| ${"wubr"}
-    ${"Artifice"}   | ${"wubr"}
-    ${"Sans White"} | ${"ubrg"}
-    ${"Sans Blue"}  | ${"wbrg"}
-    ${"Sans Black"} | ${"wurg"}
-    ${"Sans Red"}   | ${"wubg"}
-    ${"Sans Green"} | ${"wubr"}
-    ${"Sans-White"} | ${"ubrg"}
-    ${"Sans-Blue"}  | ${"wbrg"}
-    ${"Sans-Black"} | ${"wurg"}
-    ${"Sans-Red"}   | ${"wubg"}
-    ${"Sans-Green"} | ${"wubr"}
-    ${"Five Color"} | ${"wubrg"}
-    ${"FiveColor"}  | ${"wubrg"}
-    ${"Five Colour"}| ${"wubrg"}
-    ${"FiveColour"} | ${"wubrg"}
+    name             | expectedResult
+    ${"c"}           | ${"c"}
+    ${"Colorless"}   | ${"c"}
+    ${"White"}       | ${"w"}
+    ${"Blue"}        | ${"u"}
+    ${"Black"}       | ${"b"}
+    ${"Red"}         | ${"r"}
+    ${"Green"}       | ${"g"}
+    ${"MonoWhite"}   | ${"w"}
+    ${"MonoBlue"}    | ${"u"}
+    ${"MonoBlack"}   | ${"b"}
+    ${"MonoRed"}     | ${"r"}
+    ${"MonoGreen"}   | ${"g"}
+    ${"Mono White"}  | ${"w"}
+    ${"Mono Blue"}   | ${"u"}
+    ${"Mono Black"}  | ${"b"}
+    ${"Mono Red"}    | ${"r"}
+    ${"Mono Green"}  | ${"g"}
+    ${"Mono-White"}  | ${"w"}
+    ${"Mono-Blue"}   | ${"u"}
+    ${"Mono-Black"}  | ${"b"}
+    ${"Mono-Red"}    | ${"r"}
+    ${"Mono-Green"}  | ${"g"}
+    ${"Azorius"}     | ${"wu"}
+    ${"Dimir"}       | ${"ub"}
+    ${"Rakdos"}      | ${"br"}
+    ${"Gruul"}       | ${"rg"}
+    ${"Selesnya"}    | ${"wg"}
+    ${"Orzhov"}      | ${"wb"}
+    ${"Izzet"}       | ${"ur"}
+    ${"Golgari"}     | ${"bg"}
+    ${"Boros"}       | ${"wr"}
+    ${"Simic"}       | ${"ug"}
+    ${"Naya"}        | ${"wrg"}
+    ${"Esper"}       | ${"wub"}
+    ${"Grixis"}      | ${"ubr"}
+    ${"Jund"}        | ${"brg"}
+    ${"Bant"}        | ${"wug"}
+    ${"Abzan"}       | ${"wbg"}
+    ${"Temur"}       | ${"urg"}
+    ${"Jeskai"}      | ${"wur"}
+    ${"Mardu"}       | ${"wbr"}
+    ${"Sultai"}      | ${"bug"}
+    ${"Glint"}       | ${"ubrg"}
+    ${"Glinteye"}    | ${"ubrg"}
+    ${"Glint Eye"}   | ${"ubrg"}
+    ${"Glint-Eye"}   | ${"ubrg"}
+    ${"Chaos"}       | ${"ubrg"}
+    ${"Dune"}        | ${"wbrg"}
+    ${"Dunebrood"}   | ${"wbrg"}
+    ${"Dune Brood"}  | ${"wbrg"}
+    ${"Dune-Brood"}  | ${"wbrg"}
+    ${"Aggression"}  | ${"wbrg"}
+    ${"Ink"}         | ${"wurg"}
+    ${"Inktreader"}  | ${"wurg"}
+    ${"Ink-Treader"} | ${"wurg"}
+    ${"Ink Treader"} | ${"wurg"}
+    ${"Altruism"}    | ${"wurg"}
+    ${"Witch"}       | ${"wubg"}
+    ${"Witchmaw"}    | ${"wubg"}
+    ${"Witch-Maw"}   | ${"wubg"}
+    ${"Witch Maw"}   | ${"wubg"}
+    ${"Growth"}      | ${"wubg"}
+    ${"Yore"}        | ${"wubr"}
+    ${"Yoretiller"}  | ${"wubr"}
+    ${"Yore Tiller"} | ${"wubr"}
+    ${"Yore-Tiller"} | ${"wubr"}
+    ${"Artifice"}    | ${"wubr"}
+    ${"Sans White"}  | ${"ubrg"}
+    ${"Sans Blue"}   | ${"wbrg"}
+    ${"Sans Black"}  | ${"wurg"}
+    ${"Sans Red"}    | ${"wubg"}
+    ${"Sans Green"}  | ${"wubr"}
+    ${"Sans-White"}  | ${"ubrg"}
+    ${"Sans-Blue"}   | ${"wbrg"}
+    ${"Sans-Black"}  | ${"wurg"}
+    ${"Sans-Red"}    | ${"wubg"}
+    ${"Sans-Green"}  | ${"wubr"}
+    ${"Five Color"}  | ${"wubrg"}
+    ${"FiveColor"}   | ${"wubrg"}
+    ${"Five Colour"} | ${"wubrg"}
+    ${"FiveColour"}  | ${"wubrg"}
   `("reads $name as $expectedResult", ({ name, expectedResult }) => {
     expect(parseColorFromValue(name)).toEqual(expectedResult.split(""));
   });
