dbf8c35b2cb1b3b487bb738f236ec80d0fb53596
Blade Barringer <blade@crookedneighbor.com>
2021-03-17 21:00:36 -0500

    feat: allow braces in string normalization

--- test/unit/normalize-string-input.test.ts
+++ test/unit/normalize-string-input.test.ts
@@ -7,10 +7,10 @@ describe("normalizeStringInput", () => {
     expect(result).toBe("asdf");
   });
 
-  it("removes all non-alphanumeric and space characters", async () => {
-    const result = await normalizeStringInput("a3_! B2");
+  it("removes all non-alphanumeric and space and brace characters", async () => {
+    const result = await normalizeStringInput("a3_!  {T} B@2~^");
 
-    expect(result).toBe("a3 b2");
+    expect(result).toBe("a3  {t} b2");
   });
 
   it("trims trailing space, but preserves inner space", async () => {
